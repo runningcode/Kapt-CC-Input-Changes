1b043506a5135a11cd910999ef9a0261ea307c6b
Nelson Osacky <nosacky@gradle.com>
2023-04-21 15:49:35 +0200

    Initial commit

---
+++ app/src/main/java/com/osacky/cc/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.osacky.cc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.osacky.cc.ui.theme.CCInputChangesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CCInputChangesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CCInputChangesTheme {
+        Greeting("Android")
+    }
+}
---
+++ mylibrary/src/main/java/com/osacky/mylibrary/Orange.kt
@@ -0,0 +1,5 @@
+package com.osacky.mylibrary
+
+class Orange {
+
+}
